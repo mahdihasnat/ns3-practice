28fe0b9a05402e224563d5b47a517d5006fcff03
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2022-01-31 18:41:41 +0600

    Created tora module

---
+++ ns-3.35/contrib/tora/helper/tora-helper.h
@@ -0,0 +1,14 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef TORA_HELPER_H
+#define TORA_HELPER_H
+
+#include "ns3/tora.h"
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* TORA_HELPER_H */
+
---
+++ ns-3.35/contrib/tora/model/tora.h
@@ -0,0 +1,17 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef TORA_H
+#define TORA_H
+
+namespace ns3 {
+
+/* ... */
+
+namespace tora {
+
+
+} // namespace tora
+
+} // namespace ns3
+
+#endif /* TORA_H */
+
